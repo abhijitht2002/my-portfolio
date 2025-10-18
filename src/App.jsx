5f280bbfcc692b66829e6803cc16d5bf9be0bd03
abhijith <abhijitht210@gmail.com>
2025-10-18 21:21:15 +0530

    nil

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import ProfileCard from "./components/ProfileCard";
 
 function App() {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <ProfileCard />
     </div>
   );
 }
---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ProfileCard() {
+  return (
+    <div>
+        <img src="" alt="" />
+        <h1>Abhijith T</h1>
+        <p>Aspiring Web Developer</p>
+    </div>
+  )
+}
+
+export default ProfileCard
