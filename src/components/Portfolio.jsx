1cc5340f115aac450996c98c74d4826552e75ef7
abhijith <abhijitht210@gmail.com>
2025-11-08 20:33:12 +0530

    nil

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,18 +2,50 @@ import React from "react";
 
 function Portfolio() {
   return (
-    <div className="flex flex-col">
-      <div className="w-full lg:max-w-2xl mx-auto bg-white p-6 shadow-sm rounded-xl">
-        <h1>Hey everyone 👋</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eum
-          architecto repellat voluptatibus itaque nobis dolorum consequuntur
-          dolore a iusto! Iure natus tempora eius minima aliquam culpa eaque
-          aperiam nemo!
-        </p>
-        <p className="mt-2 text-blue-800">
-          #Java #WebDevelopment #SpringBoot #LearningJourney #DeveloperLife
-        </p>
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col">
+        <div className="w-full lg:max-w-2xl mx-auto bg-white p-6 shadow-sm rounded-xl">
+          <h1>Hey everyone 👋</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eum
+            architecto repellat voluptatibus itaque nobis dolorum consequuntur
+            dolore a iusto! Iure natus tempora eius minima aliquam culpa eaque
+            aperiam nemo!
+          </p>
+          <p className="tags mt-2 text-blue-800">
+            #Java #WebDevelopment #SpringBoot #LearningJourney #DeveloperLife
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="w-full lg:max-w-2xl mx-auto bg-white p-6 shadow-sm rounded-xl">
+          <h1>Bachelor of Computer Application</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eum
+            architecto repellat voluptatibus itaque nobis dolorum consequuntur
+            dolore a iusto! Iure natus tempora eius minima aliquam culpa eaque
+            aperiam nemo!
+          </p>
+          <p className="tags mt-2 text-blue-800">
+            #Java #WebDevelopment #SpringBoot #LearningJourney #DeveloperLife
+          </p>
+        </div>
+      </div>
+      
+      <div className="flex flex-col">
+        <div className="w-full lg:max-w-2xl mx-auto bg-white p-6 shadow-sm rounded-xl">
+          <h1>Hey everyone 👋</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus eum
+            architecto repellat voluptatibus itaque nobis dolorum consequuntur
+            dolore a iusto! Iure natus tempora eius minima aliquam culpa eaque
+            aperiam nemo!
+          </p>
+          <p className="tags mt-2 text-blue-800">
+            #Java #WebDevelopment #SpringBoot #LearningJourney #DeveloperLife
+          </p>
+        </div>
       </div>
     </div>
   );
